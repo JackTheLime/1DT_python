cfe473696185f352fec5495fcaa648e744d51989
Jack Leong <99371792+JackTheLime@users.noreply.github.com>
2022-08-16 10:39:20 +1200

    Add files via upload

---
+++ python_practice_tasks/Python_Practice1_Jack_Leong.py
@@ -0,0 +1,25 @@
+
+#This is a STRING variable and is shown by the quotation marks.#
+name="Jack Leong"
+
+#This is a FLOAT variable, this is shown by the decimal point.#
+height=168.81
+
+#This is a STRING variable and is shown by the quotation marks.#
+address="79 Ludlam Street"
+
+#This is a STRING variable and is shown by the quotation marks.#
+phonenumber="02108779479"
+
+#This is a BOOL variable, this is shown by having a value of either true or false.#
+allergy=False
+
+#This is the defined variables put into one simple sentence.#
+print("My name is", name, ", I am", height, "centimetres tall, I live at", address, ". To contact me, call", phonenumber, "and do I have an allergy?", allergy, ".")
+
+#These print out what the types of variables these are once ran.#
+print(type(name))
+print(type(height))
+print(type(address))
+print(type(phonenumber))
+print(type(allergy))
